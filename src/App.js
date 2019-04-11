efe408dba25a281dff9e2b080d4573570c72120e
Pob <590650@gmail.com>
2019-04-11 23:37:27 +0700

    import styled component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useWindowResize } from './useWindowResize'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
 
 const App = () => {
   const { width, height } = useWindowResize()
   return (
-    <div>
-      <p>Window width is: {width}</p>
-      <p>Window height is: {height}</p>
-    </div>
+    <Container>
+      <h1>Window width is: {width}</h1>
+      <h1>Window height is: {height}</h1>
+    </Container>
   )
 }
 
