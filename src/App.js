6f712ff9528e56271dd02ad7fa7a224ba131aa63
Pob <590650@gmail.com>
2019-04-12 18:44:48 +0700

    restructure styled component into one file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react'
 import { useWindowResize } from './useWindowResize'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background-color: #333;
-`
-
-const H1 = styled.h1`
-  color: hotpink;
-`
+import * as S from './styled'
 
 const App = () => {
   const { width, height } = useWindowResize()
   return (
-    <Container>
-      <H1>Window width is: {width}</H1>
-      <H1>Window height is: {height}</H1>
-    </Container>
+    <S.FlexContainer>
+      <S.Title>Window width is: {width}</S.Title>
+      <S.Title>Window height is: {height}</S.Title>
+    </S.FlexContainer>
   )
 }
 
---
+++ src/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #333;
+`
+
+export const Title = styled.h1`
+  color: hotpink;
+`
